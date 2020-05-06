e60d02552f6f9dabdcd5437610ce2660612faf0b
Nick Hodges <nickhodges@gmail.com>
2020-05-05 22:20:16 -0400

    Import stuff that needed to be imported

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 
 import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PassportModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      signOptions: { expiresIn: '600m' },
+    }),
+  ],
   providers: [JwtStrategy],
-  exports: [JwtStrategy]
+  exports: [PassportModule],
 })
 export class AuthModule {}

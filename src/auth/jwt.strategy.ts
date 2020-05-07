3ba64b43c5748a0313569239866e8f6c40583b5c
Nick Hodges <nickhodges@gmail.com>
2020-05-06 20:15:22 -0400

    Use jwt strategy straight from working demo

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,8 +1,10 @@
-import { passportJwtSecret } from 'jwks-rsa';
-import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
-
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { passportJwtSecret } from 'jwks-rsa';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,21 +14,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+        jwksUri: `${process.env.AUTH0_DOMAIN}.well-known/jwks.json`,
       }),
 
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      audience: process.env.AUTH_AUDIENCE,
-      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: `${process.env.AUTH0_DOMAIN}`,
+      algorithms: ['RS256'],
     });
   }
 
-  validate(payload: any, done: VerifiedCallback) {
-    console.log('validating....');
-    if (!payload) {
-      done(new UnauthorizedException(), false);
-    }
-
-    return done(null, payload);
+  validate(payload: any) {
+    return payload;
   }
 }

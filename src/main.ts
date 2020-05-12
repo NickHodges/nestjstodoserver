8f5aa6b084e5adf6d88025134179515b7a034cd4
Nick Hodges <nickhodges@gmail.com>
2020-05-11 21:42:54 -0400

    Put key files in .env file, though at this point it is kind of moot because the names are already in the repo.

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { AppModule } from './app.module';
 import * as fs from 'fs';
 //import passport from 'passport';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const httpsOptions = {
-    key: fs.readFileSync('./Nicklocalhost.key'),
-    cert: fs.readFileSync('./Nicklocalhost.crt'),
+    key: fs.readFileSync(`./${process.env.KEYFILE}`),
+    cert: fs.readFileSync(`./${process.env.CRTFILE}`),
   };
+
   const TodoServer = await NestFactory.create(AppModule, { httpsOptions });
   TodoServer.enableCors({
     credentials: true,
@@ -17,7 +21,7 @@ async function bootstrap() {
   TodoServer.useGlobalPipes(
     new ValidationPipe({
       disableErrorMessages: true,
-    }),
+    })
   );
 
   await TodoServer.listen(process.env.PORT);
